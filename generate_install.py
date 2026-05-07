a8cfee0cd3e9ac157528fac28a96c4a30c0a3714
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2026-05-07 23:59:22 +0200

    BF [Tutorial]: Add libsox-fmt-mp3 to display audio information

--- generate_install.py
+++ generate_install.py
@@ -12,7 +12,7 @@ def generate_install_commands():
 
     # Write to markdown file
     markdown_content = f"""```python
-!apt-get install tree netbase sox
+!apt-get install tree netbase sox libsox-fmt-mp3
 !{pip_command}
 ```"""
 
